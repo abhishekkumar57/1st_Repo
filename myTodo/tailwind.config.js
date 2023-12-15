6cfe320db0b3132d8bd9c3a795b5493db952487c
Abhishek <149564249+abhishekkumar57@users.noreply.github.com>
2023-12-15 07:38:55 +0000

    chekkk

---
+++ myTodo/tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+export const content = [
+  "./src/**/*.{html,ts}",
+];
+export const theme = {
+  colors: {
+    'sky': '#006D77',
+  },
+  extend: {},
+};
+export const plugins = [];
