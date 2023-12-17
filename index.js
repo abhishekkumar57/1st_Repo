8477ebe4ff7bbef0e36b575e5285dc96a57985e1
Abhishek <149564249+abhishekkumar57@users.noreply.github.com>
2023-12-17 15:21:49 +0000

    chang

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-let total_rows=5;
-for(let i=1;i<total_rows;i++){
-    for(let j=1;j<total_rows;j++)
-    {
-        process.stdout.write("*")
-        
-    }
-    console.log()
+// let total_rows=5;
+// for(let i=1;i<total_rows;i++){
+//     for(let j=1;j<total_rows;j++)
+//     {
+//         process.stdout.write("*")
+
+//     }
+//     console.log()
+// }
+
+// Add All element of the given question..
+
+let x = 5321;
+let sum = 0;
+while (x > 0) {
+    let lastdigits = x % 10;
+    sum += lastdigits;
+
+    x = Math.floor(x / 10);
 }
+console.log(sum)
+
